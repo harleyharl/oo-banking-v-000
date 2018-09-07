2181c93e70d9599b8548be0f7c747bb1f9318684
Harley Rayner <harleymteden@gmail.com>
2018-09-07 19:29:51 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ class Transfer
    end
 
   def reverse_transfer
-    if self.status = "complete"
+    if @status = "complete"
       @sender.balance = @sender.balance + @amount
       @receiver.balance = @receiver.balance - @amount
       @status = "reversed"
