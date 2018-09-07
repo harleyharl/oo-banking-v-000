6eb31a1826d46b0d416b3ea231a2d349c9b71f4d
Harley Rayner <harleymteden@gmail.com>
2018-09-07 19:21:50 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
         @status = "complete"
       elsif
          valid? && @sender.balance < @amount
-        @status = "Transaction rejected. Please check your account balance."
+        @status = "rejected."
       else
         @status = "rejected."
       end
