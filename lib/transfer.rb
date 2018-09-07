0356bb4331b3e2c9802e2e452d87294c032785e4
Harley Rayner <harleymteden@gmail.com>
2018-09-07 17:53:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
 
   def execute_transaction
       if valid?
-        binding.pry
+        # binding.pry
         @sender.balance = @sender.balance - @amount
         @receiver.balance = @receiver.balance + @amount
         @status = "complete"
