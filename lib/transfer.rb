e7ce05edcd3d47887c92ae9dac831896d4ec42f8
Harley Rayner <harleymteden@gmail.com>
2018-09-07 18:02:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,21 +13,21 @@ class Transfer
   def valid?
     if @status != "complete"
     @sender.valid? && @receiver.valid?
-    @sender.balance > @amount
+    # @sender.balance > @amount
     end
   end
 
   def execute_transaction
-      if valid?
+      if self.valid? && @sender.balance > @amount
         # binding.pry
         @sender.balance = @sender.balance - @amount
         @receiver.balance = @receiver.balance + @amount
         @status = "complete"
       elsif
         valid? && @sender.balance < @amount
-        @status = "Transaction rejected. Please check your account balance."
-      else
         @status = "rejected."
+      else
+        @status = "Transaction rejected. Please check your account balance."
       end
    end
 
